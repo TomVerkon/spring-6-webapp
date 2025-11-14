5d41e25c131340460103fffef665fbf306050a4b
TomVerkon <tverkon@gmail.com>
2025-11-14 14:22:42 -0500

    add additional authors and books to BootstrapData for data seeding

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -33,8 +33,8 @@ public class BootstrapData implements CommandLineRunner {
     book.setIsbn("123123");
 
     Author eric = authorRepository.save(author);
-
     Book ddd = bookRepository.save(book);
+
     eric.getBooks().add(ddd);
     eric = authorRepository.save(eric);
     ddd.getAuthors().add(eric);
@@ -42,6 +42,25 @@ public class BootstrapData implements CommandLineRunner {
 
     System.out.println("Book: " + ddd.getAuthors());
     System.out.println("Author: " + eric.getBooks());
+
+    author = new Author();
+    author.setFirstName("Rod");
+    author.setLastName("Johnson");
+
+    book = new Book();
+    book.setTitle("J2EE Development without EJB");
+    book.setIsbn("456456");
+
+    Author rod = authorRepository.save(author);
+
+    Book J2EE = bookRepository.save(book);
+    rod.getBooks().add(J2EE);
+    rod = authorRepository.save(rod);
+    J2EE.getAuthors().add(rod);
+    J2EE = bookRepository.save(J2EE);
+
+    System.out.println("Book: " + J2EE.getAuthors());
+    System.out.println("Author: " + rod.getBooks());
 
     // add bootstrap logic here
     System.out.println("Bootstrapping data...");
