2ce35921d5f1ebb282bdf50803fa0c9789d2079a
TomVerkon <tverkon@gmail.com>
2025-11-15 20:55:49 -0500

    Enhance BookController with improved request mapping and logging, update dependencies to include JetBrains annotations

--- src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
@@ -1,13 +1,14 @@
 package guru.springframework.spring6webapp.controllers;
 
+import guru.springframework.spring6webapp.domain.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springframework.spring6webapp.services.BookService;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/books")
 public class BookController {
   private final BookService bookService;
 
@@ -15,8 +16,11 @@ public class BookController {
     this.bookService = bookService;
   }
 
-  public String getBooks(Model model) {
-    model.addAttribute("books", bookService.findAll());
-    return "books";
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public String getBooks(Model model) {
+      Iterable<Book> books = bookService.findAll();
+      model.addAttribute("books", books);
+      System.out.println(books);
+      return "books";
   }
 }
